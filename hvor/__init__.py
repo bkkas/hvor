3f9031d5edb59b1023994e94d7ee1abe1f9f4941
Pål Grønås Drange <paal.drange+pgdr@gmail.com>
2021-08-29 20:13:34 +0200

    Add console script to hvor (#10)
    A new file hvor.console adds a function main() that when called, reads
    two points from sys.argv and prints the json to standard out.

--- hvor/__init__.py
+++ hvor/__init__.py
@@ -1,1 +1,2 @@
 from hvor.hvor import point, points
+from hvor.console import main
---
+++ hvor/console.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import sys
+import json
+from .hvor import point
+
+
+def _exit_with_usage(msg=None):
+    if not msg:
+        print("Usage: hvor lat lon")
+        sys.exit()
+    else:
+        # exit code = 1 when sys.exit receives message
+        sys.exit(msg)
+
+
+def _print_point_to_json(lat: float, lon: float) -> None:
+    """This function is called when hvor is called from the console.
+    It prints the point dict to std.out.
+    """
+    pt = point(lat, lon)
+    print(json.dumps(pt, indent=4))
+
+
+def main():
+    if "-h" in sys.argv or "--help" in sys.argv:
+        _exit_with_usage()
+    if len(sys.argv) != 3:
+        _exit_with_usage("Usage: hvor lat lon")
+    s1 = sys.argv[1]
+    s2 = sys.argv[2]
+    try:
+        f1, f2 = float(s1), float(s2)
+    except ValueError as err:
+        msg = f"hvor takes two floating point numbers: {err}"
+        _exit_with_usage(msg)
+    _print_point_to_json(f1, f2)
+
+
+if __name__ == "__main__":
+    main()
